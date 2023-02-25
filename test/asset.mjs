9148cc0cfef1f82271f1dbe0832a8302632bfbc4
legend80s <liuchzong@qq.com>
2023-02-26 00:22:41 +0800

    test: test random word explain not throw error

---
+++ test/asset.mjs
@@ -0,0 +1,44 @@
+import fs from 'node:fs';
+const fp = './test/asset.txt';
+
+const words = Array.from(new Set(fs.readFileSync(fp).toString('utf8')
+  .toLowerCase()
+  .match(/[^-<>"\s,.:+#\/\\()@]+/g)));
+
+// 2279
+// 895
+// console.log('words.length:', words.length);
+// 825
+
+export const pickRandomWords = (limit) => {
+  return pickRandoms(words, limit)
+}
+
+/**
+ *
+ * @param {any[]} arr
+ * @param {number} limit
+ * @returns {any[]}
+ */
+function pickRandoms(arr, limit) {
+  const set = new Set();
+
+  for (let index = 0; index < arr.length; index++) {
+    const idx = randomInteger(0, arr.length);
+
+    if (!set.has(arr[idx])) {
+      set.add(arr[idx])
+
+      if (set.size === limit) {
+        return set;
+      }
+    }
+  }
+
+  return set;
+}
+
+function randomInteger(min, max) {
+  // [0, 1)
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
---
+++ test/core.test.random.mjs
@@ -0,0 +1,18 @@
+import test from 'node:test';
+import assert from 'node:assert';
+import { execSync } from 'node:child_process';
+import { pickRandomWords } from './asset.mjs';
+
+test('should not throw error on random word', () => {
+  const limit = 5;
+  const randomWords = pickRandomWords(limit)
+
+  console.log('randomWords:', randomWords);
+  assert.equal(randomWords.size, limit)
+
+  for (const word of randomWords) {
+    assert.doesNotThrow(() => {
+      execSync(`node ./ ${word} -e`).toString('utf-8');
+    })
+  }
+})
