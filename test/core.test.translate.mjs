3f4da2482b4eccfb57f6839b8fcfc8e40a6b8139
legend80s <liuchzong@qq.com>
2023-03-12 00:24:33 +0800

    test: add translator test

--- test/core.test.translate.mjs
+++ test/core.test.translate.mjs
@@ -1,20 +1,13 @@
-// import { translate } from '../src/translator/index.mjs';
+import { translate } from '../src/translator/engines/baidu.mjs';
 
-// import test from 'node:test';
-// import assert from 'node:assert';
+import test from 'node:test';
+import assert from 'node:assert';
 
-// test('should translate en to zh', async () => {
-//   const text = 'In reality, there are also implicit implementations of FnMut and FnOnce for Closure, but Fn is the “fundamental” one for this closure.';
-//   const expected = "实际上，对于闭包也有fnmut和FnOnce的隐式实现，但Fn是这个闭包的\"基础\"。";
+test('should translate en to zh', async () => {
+  const text = 'In reality, there are also implicit implementations of FnMut and FnOnce for Closure, but Fn is the “fundamental” one for this closure.';
+  const expected = "实际上，对于闭包也有FnMut和FnOnce的隐式实现，但Fn是这个闭包的“基本”实现。";
 
-//   let actual = '';
-//   console.time('translate')
+  const actual = await translate(text);
 
-//   try {
-//     actual = await translate(text);
-//   } finally {
-//     console.timeEnd('translate')
-//   }
-
-//   assert.equal(actual, expected);
-// })
+  assert.equal(actual, expected);
+})
