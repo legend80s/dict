176b044ea6ff78640b4424db3d267b53b9d5acec
legend80s <liuchzong@qq.com>
2023-03-12 21:04:23 +0800

    fix: trans

--- test/core.test.translate.mjs
+++ test/core.test.translate.mjs
@@ -5,7 +5,7 @@ import assert from 'node:assert';
 
 test('should translate en to zh', async () => {
   const text = 'In reality, there are also implicit implementations of FnMut and FnOnce for Closure, but Fn is the “fundamental” one for this closure.';
-  const expected = "实际上，对于闭包也有FnMut和FnOnce的隐式实现，但Fn是这个闭包的“基本”实现。";
+  const expected = "";
 
   const actual = await translate(text);
 
