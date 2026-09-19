9b7671806a499fc6d3e9357b6c62229d1cb21137
legend80s <liuchzong@qq.com>
2026-09-19 13:59:03 +0800

    fix: test failed when no word found

--- test/core.test.random.mjs
+++ test/core.test.random.mjs
@@ -7,22 +7,28 @@ import { disableStream } from './global-setup-teardown.mjs'
 disableStream()
 
 test('should not throw error on random word', () => {
-  const limit = 5
-  const randomWords = pickRandomWords(limit)
+  const limit = 2
+  const notFondWord = 'createelement'
+  const randomWords = pickRandomWords(limit).add(notFondWord)
 
   console.info('randomWords:', randomWords)
-  assert.equal(randomWords.size, limit)
+  assert.equal(randomWords.size, limit + 1)
 
   for (const word of randomWords) {
     console.info('word:', `[${word}]`)
 
     assert.doesNotThrow(() => {
-      const cmd = `node ./bin.mjs ${word} -e`
+      const cmd = `node ./bin.mjs ${word} -e 2>&1`
       // console.log('cmd:', { cmd })
-
       const stdout = execSync(cmd).toString('utf-8')
 
-      assert.match(stdout, /See more at/)
+      // console.log(`stdout:|${stdout}|`)
+
+      try {
+        assert.match(stdout, /See more at/)
+      } catch {
+        assert.match(stdout, /抱歉没有找到“\w+”相关的词/)
+      }
     })
   }
 })
